1a30705ceb206fd27a989765fdfcb88f6c8b75ba
Sodiq Sanusi <81825032+sodiqsanusi@users.noreply.github.com>
2022-09-23 20:06:29 +0100

    Time: 181 ms (92.84%), Space: 51.2 MB (43.86%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,5 +3,5 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-   return x.toString().split('').reverse().join('') == x.toString()
+    return x.toString().split('').reverse().join('') == x.toString()
 };
