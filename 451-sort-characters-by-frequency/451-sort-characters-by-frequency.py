d2b2f77ffa31786a7f25bcad97e474c4256124db
Sodiq Sanusi <81825032+sodiqsanusi@users.noreply.github.com>
2022-09-25 16:03:12 +0100

    Time: 64 ms (72.24%), Space: 15.2 MB (80.91%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -0,0 +1,9 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        lilac = set(s)
+        final = []
+        for char in lilac:
+            final.append(char * (s.count(char)))
+            final.sort(key=len, reverse=True)
+        return (''.join(final))
+        
